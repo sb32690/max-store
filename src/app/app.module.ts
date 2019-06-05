ac6a9220959a955223a7995099832e737024aeb2
sbaxley <sb32690@gmail.com>
2019-06-05 09:52:33 -0400

    Recipe Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingComponent } from './shopping-list/shopping/shopping.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component'
 import { RecipeListComponent} from './recipe-book/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component'
@@ -13,8 +14,9 @@ import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingComponent,
+    ShoppingListComponent,
     ShoppingEditComponent,
+    RecipeBookComponent,
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shopping-list-app',
+  templateUrl: './shopping-list.component.html'
+})
+export class ShoppingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
